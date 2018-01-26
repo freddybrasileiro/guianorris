ebd29404d1c0d49384b3082045eb8b1b40e45b42
Freddy Brasileiro <freddybrasileiro@users.noreply.github.com>
2018-01-26 18:48:18 -0200

    spelling mistake

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -14,7 +14,7 @@ class MainLayout extends Component {
                   <img className="align-self-center mr-4 rounded-circle" src="chuck-norris-avatar.jpg" alt="Chuck Norris" width="100" />
                   <div className="align-self-center media-body">
                     <h3 className="mt-0">Welcome to Guia Norris</h3>
-                    <h5>where Guia Bolso family follow Chuck Norris</h5>
+                    <h5>where Guia Bolso family follows Chuck Norris</h5>
                   </div>
                 </div>
               </div>
